726afd8d149d40d13b8212e62388ce8571dadc03
Alexander Tarasov <oioki@yandex.ru>
2015-01-29 16:36:07 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -2,12 +2,20 @@
 
 require("authcommon.php");
 
-if ( !isset($_COOKIE['auth']) ) Header('Location: login.html');
+if ( !isset($_COOKIE['auth']) )
+{
+    Header('Location: login.html');
+    exit;
+}
 
 list($login,$pass) = split(":",$_COOKIE['auth']);
 $userid = check_credentials($login,$pass);
 
-if ( !$userid ) Header('Location: login.html');
+if ( !$userid )
+{
+    Header('Location: login.html');
+    exit;
+}
 
 $smarty->assign('admin',$admin);
 
